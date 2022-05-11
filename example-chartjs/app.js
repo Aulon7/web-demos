e00afa32c93374c63269f28b1d81b7c109f504f2
Aulon Morina <aulonmorina@gmail.com>
2022-05-11 11:51:08 +0200

    formatted

--- example-chartjs/app.js
+++ example-chartjs/app.js
@@ -1,112 +1,153 @@
 // Line Chart
 
-const ctx = document.getElementById('myChart1');
+const ctx = document.getElementById("myChart1");
 const myChart1 = new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
-      labels: ['2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'],
-      datasets: [{
-      label: 'Average FICO Score',
-      backgroundColor: 'rgb(117, 177, 219)',
-      borderColor: 'rgb(45, 124, 180)',
-      data: [688, 690, 689, 689, 686, 687, 689, 689, 690, 694, 695, 699, 701,705, 706, 713, 716],
-    }]
-
+    labels: [
+      "2005",
+      "2006",
+      "2007",
+      "2008",
+      "2009",
+      "2010",
+      "2011",
+      "2012",
+      "2013",
+      "2014",
+      "2015",
+      "2016",
+      "2017",
+      "2018",
+      "2019",
+      "2020",
+      "2021",
+    ],
+    datasets: [
+      {
+        label: "Average FICO Score",
+        backgroundColor: "rgb(117, 177, 219)",
+        borderColor: "rgb(45, 124, 180)",
+        data: [
+          688, 690, 689, 689, 686, 687, 689, 689, 690, 694, 695, 699, 701, 705,
+          706, 713, 716,
+        ],
+      },
+    ],
   },
-
 });
 
 // Pie Chart
 
-let data = [{
-  data: [35, 30, 15,10,10],
-      backgroundColor: [
-        'rgb(231, 76, 60)',
-        'rgb(241, 196, 15)',
-        'rgb(39, 174, 96)',
-        'rgb(52, 152, 219)',
-        'rgb(155, 89, 182)'
-      ],
-      hoverOffset: 3
-  }];
+let data = [
+  {
+    data: [35, 30, 15, 10, 10],
+    backgroundColor: [
+      "rgb(231, 76, 60)",
+      "rgb(241, 196, 15)",
+      "rgb(39, 174, 96)",
+      "rgb(52, 152, 219)",
+      "rgb(155, 89, 182)",
+    ],
+    hoverOffset: 3,
+  },
+];
 
 let options = {
   plugins: {
     datalabels: {
       formatter: (value, ctx2) => {
-        const datapoints = ctx2.chart.data.datasets[0].data
-         const total = datapoints.reduce((total, datapoint) => total + datapoint, 0)
-        const percentage = value / total * 100
+        const datapoints = ctx2.chart.data.datasets[0].data;
+        const total = datapoints.reduce(
+          (total, datapoint) => total + datapoint,
+          0
+        );
+        const percentage = (value / total) * 100;
         return percentage.toFixed(1) + "%";
       },
-      color: '#fff',
+      color: "#fff",
     },
     tooltip: {
-      enabled: false
+      enabled: false,
     },
     labels: {
-      render: 'percentage',
-
-    }
-  }
+      render: "percentage",
+    },
+  },
 };
 
 let ctx2 = document.getElementById("myChart2");
 let myChart2 = new Chart(ctx2, {
-  type: 'pie',
+  type: "pie",
   data: {
-  labels: ["Payment History","Amounts owed","Length of credit history", "Credit mix","New credit"],
-    datasets: data
+    labels: [
+      "Payment History",
+      "Amounts owed",
+      "Length of credit history",
+      "Credit mix",
+      "New credit",
+    ],
+    datasets: data,
   },
   options: options,
   plugins: [ChartDataLabels],
 });
 
-
 // Bar Chart
 let ctx3 = document.getElementById("myChart3");
 let myChart3 = new Chart(ctx3, {
-  type: 'bar',
+  type: "bar",
   data: {
-    labels: ["Age 18 to 23", "Age 24 to 39", "Age 40 to 55", "Age 56 to 74", "Age 75+"],
-    datasets: [{
-      label: ["Average Fico Score By Age"],
-      data: [679, 686, 705, 740, 760],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)'
-      ],
-      borderWidth: 1
-    }]
-  }
+    labels: [
+      "Age 18 to 23",
+      "Age 24 to 39",
+      "Age 40 to 55",
+      "Age 56 to 74",
+      "Age 75+",
+    ],
+    datasets: [
+      {
+        label: ["Average Fico Score By Age"],
+        data: [679, 686, 705, 740, 760],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
 
-
-//Table 
+//Table
 new gridjs.Grid({
-  columns: [{
-    name: "State",
-    sort: {
-      enabled: true,
-    }
-  }, {
-    name: "Average Credit Score 2020",
-  }, {
-    name: "Average Credit Score 2021",
-  }],
+  columns: [
+    {
+      name: "State",
+      sort: {
+        enabled: true,
+      },
+    },
+    {
+      name: "Average Credit Score 2020",
+    },
+    {
+      name: "Average Credit Score 2021",
+    },
+  ],
   sort: true,
   fixedHeader: true,
-  height: '400px',
+  height: "400px",
   data: [
     ["Alabama", "686", "691"],
     ["Alaska", "714", "717"],
@@ -158,33 +199,34 @@ new gridjs.Grid({
     ["Washington", "730", "734"],
     ["West Virginia", "695", "699"],
     ["Wisconsin", "732", "735"],
-    ["Wyoming", "719", "722"]
-  ]
+    ["Wyoming", "719", "722"],
+  ],
 }).render(document.getElementById("table-wrapper"));
-
 
 // Bar Chart
 let ctx4 = document.getElementById("myChart4");
 let myChart4 = new Chart(ctx4, {
-  type: 'bar',
+  type: "bar",
   data: {
     labels: ["Low income", "Moderate income", "Middle income", "High income"],
-    datasets: [{
-      label: ["Average Fico Score By Income"],
-      data: [658, 692, 735, 774],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-      ],
-      borderWidth: 1
-    }]
-  }
+    datasets: [
+      {
+        label: ["Average Fico Score By Income"],
+        data: [658, 692, 735, 774],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
